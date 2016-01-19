8c3c937fd684b850661c065a47ff8dae67dae295
Gabriele Petronella <gabriele@buildo.io>
2016-01-19 15:38:34 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    "no-loops": require('./lib/no-loops')
+  }
+};
---
+++ tests/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var rule = require('../lib/no-loops.js');
+var RuleTester = require('eslint').RuleTester;
+
+var ruleTester = new RuleTester();
+ruleTester.run('no-loops', rule, {
+  valid: [
+    {
+      code: '[1, 2, 3].map(function (i) { console.log(i); });'
+    }
+  ],
+
+  invalid: [
+    {
+      code: 'for (var i; i <= n; i++) { console.log(i); }',
+      errors: [ { message: 'loops are not allowed' } ]
+    },
+    {
+      code: 'for (i in [1, 2, 3]) { console.log(i); }',
+      errors: [ { message: 'loops are not allowed' } ]
+    },
+    {
+      code: 'while (i <= n) { console.log(i); }',
+      errors: [ { message: 'loops are not allowed' } ]
+    },
+    {
+      code: 'do { console.log(i); } while (i <= n)',
+      errors: [ { message: 'loops are not allowed' } ]
+    }
+  ]
+});
