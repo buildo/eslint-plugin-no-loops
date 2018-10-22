55f009e71efa3e53db127dc46f5d39bed510e2ff
Josh Chisholm <joshuachisholm@gmail.com>
2018-10-22 19:41:30 +0100

    Allow loops containing await

--- tests/index.js
+++ tests/index.js
@@ -3,11 +3,29 @@
 var rule = require('../lib/no-loops.js');
 var RuleTester = require('eslint').RuleTester;
 
-var ruleTester = new RuleTester();
+var ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2018 } });
 ruleTester.run('no-loops', rule, {
   valid: [
     {
       code: '[1, 2, 3].map(function (i) { console.log(i); });'
+    },
+    {
+      code: 'async function ok() { for (var i = 0; i < n; i++) { await x[i]() } }'
+    },
+    {
+      code: 'const ok = async () => { for (const x of y) { for (const z of y) { await z() } } }'
+    },
+    {
+      code: 'async function ok() { for (const x of y) { await x() } }'
+    },
+    {
+      code: 'async function ok() { while (true) { await x() } }'
+    },
+    {
+      code: 'async function ok() { do { await x() } while (true) }'
+    },
+    {
+      code: 'const ok = async () => { for (const x of y) { for (const z in y) { await z() } } }'
     }
   ],
 
