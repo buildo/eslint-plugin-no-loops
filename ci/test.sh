961f620e21cad06adb0a3b79c30a8a30477c89da
Gabriele Petronella <gabriele@buildo.io>
2024-06-13 12:05:18 +0200

    ESLint v9

--- ci/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-yarn install --no-progress
-yarn test
--- tests/index.js
+++ tests/index.js
@@ -14,24 +14,24 @@ ruleTester.run('no-loops', rule, {
   invalid: [
     {
       code: 'for (var i; i <= n; i++) { console.log(i); }',
-      errors: [ { message: 'loops are not allowed' } ]
+      errors: [{ message: 'loops are not allowed' }]
     },
     {
       code: 'for (i in [1, 2, 3]) { console.log(i); }',
-      errors: [ { message: 'loops are not allowed' } ]
+      errors: [{ message: 'loops are not allowed' }]
     },
     {
       code: 'while (i <= n) { console.log(i); }',
-      errors: [ { message: 'loops are not allowed' } ]
+      errors: [{ message: 'loops are not allowed' }]
     },
     {
       code: 'do { console.log(i); } while (i <= n)',
-      errors: [ { message: 'loops are not allowed' } ]
+      errors: [{ message: 'loops are not allowed' }]
     },
     {
       code: 'for (i of [1, 2, 3]) { console.log(i) }',
-      parser: 'babel-eslint',
-      errors: [ { message: 'loops are not allowed' } ]
+      errors: [{ message: 'loops are not allowed' }],
+      languageOptions: { ecmaVersion: 6 }
     }
   ]
 });
